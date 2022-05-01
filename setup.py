516a34b9cbf34917bf7a05159f8fcb6b8c8e453f
Sofia Alvarez Lopez <ms.alvarezl@uniandes.edu.co>
2022-05-01 17:41:07 -0500

    Dependencias

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="Dependencias_Lab4",
     version="0.0.1",
     author="sofiaalvarezlopez",
     author_email="ms.alvarezl@uniandes.edu.co",
     description="Dependencias para el laboratorio 4 de Inteligencia de Negocios.",
-    long_description=long_description,
     url="https://github.com/sofiaalvarezlopez/Dependencias_lab4",
     packages=setuptools.find_packages(),
     classifiers=[
